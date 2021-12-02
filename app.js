d1901110a84c4acebaa3df3540c3bb026ee3cd90
Ebone Rich <rich.ebone@gmail.com>
2021-12-02 18:32:43 -0500

    functional server

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-//require("dotenv").config();
-const Express = require ("express");
-const app = Express();
-const dbConnection = require("./db");
-
-//app.use(require('./middleware/headers'));
-
-const controllers =require("./controllers");
- app.use(Express.json());
- app.use("/user", controllers.userController);
-
-//app.use(require("./middleware/validate-jwt"));
-//app.use("/journal", controllers.journalController);
-
- dbConnection.authenticate()
- .then(() => dbConnection.sync())
- .then(() => {
-    app.listen(3000, () => {
-        console.log(`[Server]: App is listening on 3000.`);
-    });
- })
- .catch((err) => {
-     console.log(`[Server]: Server crashed. Error = ${err}`);
- });
-=======
 require("dotenv").config();
 const express = require("express");
 const app = express();
@@ -47,4 +21,3 @@ dbConnection
   .catch((err) => {
     console.log(`[Server]: Server crashed. Error = ${err}`);
   });
->>>>>>> 01e2513d3a7bc80990d006ad63bd15206af5e3e6
--- middleware/validate-jwt.js
+++ middleware/validate-jwt.js
@@ -2,47 +2,6 @@ const jwt = require("jsonwebtoken");
 const { UserModel } = require("../models");
 
 const validateJWT = async (req, res, next) => {
-<<<<<<< HEAD
-    if (req.method == "OPTIONS") {
-        next();
-    } else if (
-        req.headers.authorization &&
-        req.headers.authorization.includes("Bearer")
-    ) {
-        const { authorization } = req.headers;
-        console.log("authorization -->", authorization)
-        const payload = authorization
-            ? jwt.verify(
-                authorization.includes("Bearer")
-                    ? authorization.split(" ")[1]
-                    : authorization,
-                process.env.JWT_SECRET
-            )
-            : undefined;
-
-        console.log("payload -->", payload);
-
-        if (payload) {
-            let foundUser = await UserModel.findOne({ where: { id: payload.id } });
-                console.log("foundUser -->", foundUser);
-                
-            if (foundUser) {
-                console.log("request -->", req);
-                req.user = foundUser;
-                next();
-            } else {
-                res.status(400).send({ message: "not authorized" });
-            }
-        } else {
-            res.status(401).send({ message: "Invalid Token" });
-        }
-    } else {
-        res.status(403).send({ message: "Forbidden" });
-    }
-};
-
-module.exports = validateJWT;
-=======
   if (req.method == "OPTIONS") {
     next();
   } else if (
@@ -82,4 +41,3 @@ module.exports = validateJWT;
 };
 
 module.exports = validateJWT;
->>>>>>> 01e2513d3a7bc80990d006ad63bd15206af5e3e6
