17a12c330cd11504b0029c1151aae0e0a8fae943
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-07 18:49:57 -0500

    added ssl to db.js

--- db.js
+++ db.js
@@ -1,7 +1,21 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, 
+    process.env.HOST === "local"?
+    {
     dialect: 'postgres',
-})
+}
+:
+{
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        }
+    }
+}
+)
+
 
 module.exports = sequelize;
