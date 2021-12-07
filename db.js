39140afffbc151c6b96fb597122c7a096092d583
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-07 18:58:27 -0500

    after cannot GET

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, 
-//     process.env.HOST === "local"?
-//     {
-//     dialect: 'postgres',
-// }
-// :
+    process.env.HOST === "local"?
+    {
+    dialect: 'postgres',
+}
+:
 {
     dialect: 'postgres',
     dialectOptions: {
