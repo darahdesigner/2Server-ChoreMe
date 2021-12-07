e6c9b652832819f897bc3c496e1381a3c27ab27f
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-07 18:03:33 -0500

    deleted port heroku

--- db.js
+++ db.js
@@ -4,4 +4,16 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
 })
 
+
+// if (process.env.DATABASE_URL) {
+// //the application is executed on heroku... use the postgres database
+// sequelize = new Sequelize(pross.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     logging: true //false
+// });
+// } else {
+//     //the appplication is executed on the local machine
+//     sequelize = new Sequelize("postgres://localhost:5432/Chore-Me");
+// }
 module.exports = sequelize;
