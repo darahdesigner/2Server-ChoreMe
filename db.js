4d87ec1d5a536218cde1650b22589ab5a89877cd
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-07 19:00:29 -0500

    double equals db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, 
-    process.env.HOST === "local"?
+    process.env.HOST == "local"?
     {
     dialect: 'postgres',
 }
