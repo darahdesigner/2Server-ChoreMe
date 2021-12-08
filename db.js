652ed08c42818081e7c912e191fab1b7f0d1ef3d
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-07 19:10:41 -0500

    shaved db.js

--- db.js
+++ db.js
@@ -1,18 +1,13 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, 
-    process.env.HOST ?
-    // == "local" ?
-    {
-    dialect: 'postgres',
-}
-:
+  
 {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
-            require: true,
-            rejectUnauthorized: false,
+            require: process.env.HOST?false : true,
+            rejectUnauthorized: false
         }
     }
 }
