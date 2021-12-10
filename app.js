5e0c53ee6c649b411e900a7090fcfe1107ded888
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-09 20:56:55 -0500

    pre pull commit

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const dbConnection = require('./db')
-
 const controllers = require("./controllers");
 
 app.use(express.json())
--- db.js
+++ db.js
@@ -1,27 +1,21 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
+const db = new Sequelize(process.env.DATABASE_URL,{
+   Sequelize = new Sequelize(`postgres://localhost:${PORT}/Chore-Me`)
 }
 )
-console.log(process.env.HOST);
 
+// ^^ local    V V heroku
 
-// if (process.env.DATABASE_URL) {
-// //the application is executed on heroku... use the postgres database
-// sequelize = new Sequelize(pross.env.DATABASE_URL, {
+// sequelize = new Sequelize(process.env.DATABASE_URL, {
 //     dialect: 'postgres',
-//     protocol: 'postgres',
-//     logging: true //false
-// });
-// } else {
-//     //the appplication is executed on the local machine
-//     sequelize = new Sequelize("postgres://localhost:5432/Chore-Me");
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//     }
 // }
-module.exports = sequelize;
+// }
+// )
+
+module.exports = db;
