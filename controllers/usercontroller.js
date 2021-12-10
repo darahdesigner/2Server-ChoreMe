3c9c86815b7891260e7913f638bdddbf38ae97b8
Will Harris <bharris010486@gmail.com>
2021-12-09 19:36:27 -0500

    pushing

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -5,31 +5,35 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
-    let { email, password } = req.body.user;
+    let { username, passwordhash } = req.body.user;
+  
     try {
-        const User = await UserModel.create({
-            email,
-            password: bcrypt.hashSync(password, 13),
-        });
-
-        let token = jwt.sign({ id: User.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
-        res.status(201).json({
-            message: "User successfully registered",
-            user: User,
-            sessionToken: token
-        });
+      const User = await UserModel.create({
+        username,
+        passwordhash: bcrypt.hashSync(passwordhash, 13),
+      });
+  
+      let token = jwt.sign({ id: User.id }, process.env.JWT_SECRET, {
+        expiresIn: 60 * 60 * 24,
+      });
+      res.status(201).json({
+        message: "Registration complete!",
+        user: User,
+        sessionToken: token,
+      });
     } catch (err) {
-        if (err instanceof UniqueConstraintError) {
-            res.status(409).json({
-                message: " Email already in use",
-            });
-        } else {
-            res.status(500).json({
-                message: "Failed to register user",
-            });
-        }
+      if (err instanceof UniqueConstraintError) {
+        res.status(409).json({
+          message: "Username already in use!",
+        });
+      } else {
+        res.status(500).json({
+          message: "Failed to register the User!",
+        });
+      }
     }
-});
+  });
+  
 
 
 router.post("/login", async (req, res) => {
