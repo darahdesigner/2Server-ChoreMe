722e7a4bd3303c6dbdea1c647ef92973f1f97223
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-15 15:41:05 -0500

    removing client folders

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize/dist');
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, 
     process.env.HOST =="local" ?
