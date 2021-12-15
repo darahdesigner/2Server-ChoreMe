edd480a48b3c18af1cadb6024bee7aa19dd438ab
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-14 19:14:39 -0500

    updated db.js

--- db.js
+++ db.js
@@ -1,14 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, 
-//     process.env.HOST == "local" ?
-//     {
-//     dialect: 'postgres',
-// }
-// :
-
-// ^^ for local   VVV for heroku
-{
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     // dialectOptions: {
     //     ssl: {
@@ -18,17 +10,4 @@ const sequelize = new Sequelize(process.env.DATABASE_URL,
     // }
 })
 
-// ^^ local    V V heroku
-
-sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-    }
-}
-}
-)
-
-module.exports = db;
+module.exports = sequelize;
