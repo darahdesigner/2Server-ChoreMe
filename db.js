15f3ef176168fe5d07655d53c16d8b8dee4771a5
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-14 20:38:14 -0500

    revertttt

--- db.js
+++ db.js
@@ -2,25 +2,18 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true,
-    //         rejectUnauthorized: false
-    //     }
-    // }
 })
 
-// ^^ local    V V heroku
 
-sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-    }
-}
-}
-)
-
+// if (process.env.DATABASE_URL) {
+// //the application is executed on heroku... use the postgres database
+// sequelize = new Sequelize(pross.env.DATABASE_URL, {
+//     dialect: 'postgres',
+//     protocol: 'postgres',
+//     logging: true //false
+// });
+// } else {
+//     //the appplication is executed on the local machine
+//     sequelize = new Sequelize("postgres://localhost:5432/Chore-Me");
+// }
 module.exports = sequelize;
