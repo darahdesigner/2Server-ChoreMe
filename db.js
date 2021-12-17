58f874d968b81dba67714b49de261fdfe4ffc254
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-17 16:50:20 -0500

    referenced another student package.json for versions

--- db.js
+++ db.js
@@ -18,3 +18,23 @@ const sequelize = new Sequelize(process.env.DATABASE_URL,
   )
   
   module.exports = sequelize;
+
+
+
+  // FROM package.json!
+
+  // "repository": {
+  //   "type": "git",
+  //   "url": "git+https://github.com/darahdesigner/ChoreMe-Server.git"
+  // },
+
+  // ALSO PACKAGE.JSON
+  // Under License
+  // "bugs": {
+  //   "url": "https://github.com/darahdesigner/ChoreMe-Server/issues"
+  // },
+  // "homepage": "https://github.com/darahdesigner/ChoreMe-Server#readme",
+
+// package.json again
+// in dependencies {}
+// "middleware": "^1.0.0",
