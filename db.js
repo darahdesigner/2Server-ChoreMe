bf68f42264a46bd47f177523dfb9622255b1ec44
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-17 16:12:17 -0500

    changed db a bit

--- db.js
+++ db.js
@@ -1,19 +1,15 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, 
-  //   process.env.HOST =="local" ?
-  //   {
-  //   dialect: 'postgres',
-  //   }
-  //  :
+
    {
         dialect: 'postgres',
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false
-          }
-        }
+        // dialectOptions: {
+        //   ssl: {
+        //     require: true,
+        //     rejectUnauthorized: false
+        //   }
+        // }
       }
   )
   
